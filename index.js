1e8c890d3173de949157c8f73837ea187d6af196
Tanveer Hossain Jony <thjbd19@gmail.com>
2023-07-26 09:14:47 +0600

    delete

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ async function run() {
       res.send(result);
     });
 
-    app.delete("/postClasses/:id", async (req, res) => {
+    app.delete("/allclasses/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await classesCollection.deleteOne(query);

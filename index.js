c4bc980e6d7e90031abcaae9e729d7dfd90e8276
Tanveer Hossain Jony <thjbd19@gmail.com>
2023-07-30 23:27:46 +0600

    made user to admin

--- index.js
+++ index.js
@@ -46,6 +46,19 @@ async function run() {
       res.send(result);
     });
 
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
     // instructor apis
 
     app.get("/instructors", async (req, res) => {

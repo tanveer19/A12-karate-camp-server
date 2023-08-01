a5604938358738cdfbe45df6e8c30faa8618cce1
Tanveer Hossain Jony <thjbd19@gmail.com>
2023-08-01 21:51:27 +0600

    78-10 Limit the feature based on admin level

--- index.js
+++ index.js
@@ -9,6 +9,28 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+
+  // bearer token
+  const token = authorization.split("")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3nmy0xp.mongodb.net/?retryWrites=true&w=majority`;
@@ -54,12 +76,27 @@ async function run() {
 
     app.post("/users", async (req, res) => {
       const user = req.body;
-
       const result = await usersCollection.insertOne(user);
       res.send(result);
     });
 
-    app.patch("/users/admin/:id", async (req, res) => {
+    // security layer: verifyJWT
+    // email same
+    // check admin
+    app.get("/users/admin/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+
+      if (req.decoded.email !== email) {
+        res.send({ admin: false });
+      }
+
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const result = { admin: user?.role === "admin" };
+      res.send(result);
+    });
+
+    app.patch("/users/admin/:id", verifyJWT, async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const updateDoc = {
@@ -67,6 +104,12 @@ async function run() {
           role: "admin",
         },
       };
+      const decodedEmail = req.decoded.email;
+      if (email !== decodedEmail) {
+        return res
+          .status(403)
+          .send({ error: true, message: "forbidden access" });
+      }
 
       const result = await usersCollection.updateOne(filter, updateDoc);
       res.send(result);

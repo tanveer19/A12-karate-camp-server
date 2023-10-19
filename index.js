97b192a109f90be0bf26f25d2fb1653efb94a468
Tanveer Hossain Jony <thjbd19@gmail.com>
2023-10-19 07:23:11 +0600

    78-8 (Recap) Send JWT token to back end for verification

--- index.js
+++ index.js
@@ -134,19 +134,19 @@ async function run() {
 
     // cart collection APIs
 
-    app.get("/carts", async (req, res) => {
+    app.get("/carts", verifyJWT, async (req, res) => {
       const email = req.query.email;
 
       if (!email) {
         res.send([]);
       }
 
-      // const decodedEmail = req.decoded.email;
-      // if (email !== decodedEmail) {
-      //   return res
-      //     .status(403)
-      //     .send({ error: true, message: "forbidden access" });
-      // }
+      const decodedEmail = req.decoded.email;
+      if (email !== decodedEmail) {
+        return res
+          .status(403)
+          .send({ error: true, message: "forbidden access" });
+      }
 
       const query = { email: email };
       const result = await cartCollection.find(query).toArray();
